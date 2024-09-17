de3040afc19bb06bc9580aaa0fd567e7e6e9db87
hadinhducanh <ducanhhadinh@gmail.com>
2024-09-17 20:42:20 +0700

    fix-voucher

--- src/containers/Restaurant-detail/index.tsx
+++ src/containers/Restaurant-detail/index.tsx
@@ -206,32 +206,33 @@ const RestaurantDetail: React.FC = () => {
           />
         </div>
 
-      <div className="w-full max-w-screen-xl mx-auto mt-10 px-4">
-  <h2 className="text-3xl md:text-5xl font-bold text-black">Voucher</h2>
-  <div className="relative bg-amber-100 text-black mt-6 p-6 rounded-lg shadow-lg border-2  border-black w-full md:w-2/5 mx-auto flex flex-col justify-between" style={{ height: '250px' }}>
-    {/* Phần trên (2/3) */}
-    <div className="flex-grow">
-      <p className="text-lg md:text-xl font-medium mb-4">
-        Nhập mã: <span className="font-bold text-red-600">COMTAM10</span> để được giảm 10% cho đơn hàng từ 100,000đ trở lên.
-      </p>
-      <p className="text-lg md:text-xl mb-4">
-        Hạn sử dụng: 31/12/2024
-      </p>
-    </div>
+        <div className="w-full max-w-screen-xl mx-auto mt-10 px-4">
+          <h2 className="text-3xl md:text-5xl font-bold text-black">Voucher</h2>
+          <div
+            className="relative bg-amber-100 text-black mt-6 p-6 rounded-lg shadow-lg border-2  border-black w-full md:w-2/5 mx-auto flex flex-col justify-between"
+            style={{ height: '250px' }}
+          >
+            {/* Phần trên (2/3) */}
+            <div className="flex-grow">
+              <p className="text-lg md:text-xl font-medium mb-4">
+                Nhập mã:{' '}
+                <span className="font-bold text-red-600">COMTAM10</span> để được
+                giảm 10% cho đơn hàng từ 100,000đ trở lên.
+              </p>
+              <p className="text-lg md:text-xl mb-4">Hạn sử dụng: 31/12/2024</p>
+            </div>
 
-    {/* Đường gạch đứt ngăn cách */}
-    <div className="border-t-2 border-dashed border-black my-4 w-full" ></div>
+            {/* Đường gạch đứt ngăn cách */}
+            <div className="border-t-2 border-dashed border-black my-4 w-full"></div>
 
-    {/* Phần dưới (1/3) */}
-    <div className="flex justify-center items-center">
-      <button className="bg-red-600 text-white px-6 py-2 rounded-lg font-medium text-lg hover:bg-red-700 transition duration-300">
-        COMTAM10
-      </button>
-    </div>
-  </div>
-</div>
-
-
+            {/* Phần dưới (1/3) */}
+            <div className="flex justify-center items-center">
+              <button className="bg-red-600 text-white px-6 py-2 rounded-lg font-medium text-lg hover:bg-red-700 transition duration-300">
+                COMTAM10
+              </button>
+            </div>
+          </div>
+        </div>
 
         <div className="w-full max-w-screen-xl mx-auto mt-10 px-4">
           <h2 className="text-3xl md:text-5xl font-bold text-black">
