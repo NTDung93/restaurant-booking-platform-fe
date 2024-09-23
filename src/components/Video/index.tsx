245263cb2e82baf5c43cfa69f81014fcfe83c6d3
hadinhducanh <ducanhhadinh@gmail.com>
2024-09-23 11:26:27 +0700

    img

--- src/components/Video/index.tsx
+++ src/components/Video/index.tsx
@@ -3,20 +3,24 @@ export default function Video() {
     <div className="w-4/5 mx-auto mt-5">
       <div className="flex flex-col md:flex-row">
         <div className="w-full md:w-1/2 h-96 md:h-100 bg-gray-300 flex items-center justify-center mb-4 md:mb-0">
-          <video className="w-full h-full object-cover" controls>
-            <source src="path_to_your_video.mp4" type="video/mp4" />
-            Your browser does not support the video tag.
-          </video>
+          {/* Thay video bằng img */}
+          <img
+            src="https://res.cloudinary.com/dnslrwedn/image/upload/v1727065395/image_15_kkeucl.png"
+            alt="Restaurant Experience"
+            className="w-full h-full object-cover"
+          />
         </div>
         <div className="w-full md:w-1/2 h-60 md:h-100 p-4">
           <div className="w-full h-14 text-black text-xl md:text-2xl font-bold font-['Inter'] mb-2">
             TRẢI NGHIỆM KHÔNG GIAN NHÀ HÀNG SANG TRỌNG
           </div>
           <div className="w-full h-48 md:h-80 text-black text-base md:text-xl font-normal font-['Inter'] mt-8">
-            Lorem ipsum dolor sit amet, consectetur adipisci elit, sed eiusmod
-            tempor incidunt ut labore et dolore magna aliqua. Ut enim ad minim
-            veniam, quis nostrum exercitationem ullam corporis suscipit
-            laboriosam, nisi ut aliquid ex ea commodi consequatur.
+            Khi bước vào Pizza 4P's Lê Đại Hành, bạn sẽ bị cuốn hút bởi không
+            gian vừa hiện đại vừa ấm cúng. Thiết kế mở với những khung cửa kính
+            lớn cho phép ánh sáng tự nhiên tràn ngập, tạo cảm giác thoáng đãng
+            và dễ chịu, tạo ra một môi trường cởi mở và thư giãn với các cổng
+            cong và phân vùng tạo cảm giác gần gũi hơn cho thực khách, đồng thời
+            sử dụng vật liệu tự nhiên như bê tông, gỗ và terrazzo.
           </div>
         </div>
       </div>
