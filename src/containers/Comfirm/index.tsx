3beeeb89ced44942a4852b28e16301415dbcf8a9
hadinhducanh <ducanhhadinh@gmail.com>
2024-09-24 21:09:25 +0700

    fix-comfirm

--- src/containers/Comfirm/index.tsx
+++ src/containers/Comfirm/index.tsx
@@ -7,7 +7,6 @@ export default function Comfirm() {
   const [phone] = useState('a');
   const [email, setEmail] = useState('');
   const [notes, setNotes] = useState('');
-
   return (
     <div className="flex flex-col min-h-screen">
       <div className="flex-grow">
