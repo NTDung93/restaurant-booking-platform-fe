a8a86f4b456786face364679841fe2f3cf2f0a17
hadinhducanh <ducanhhadinh@gmail.com>
2024-10-03 15:42:09 +0700

    fix-folder

---
+++ src/components/client/ImagePage/index.tsx
@@ -0,0 +1,9 @@
+import ImageComponents from '@/components/client/ImagePage/components/ImageComponents';
+
+export default function Image() {
+  return (
+    <>
+      <ImageComponents />
+    </>
+  );
+}
